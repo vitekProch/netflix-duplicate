63c283a1fdac00c45f2d7872589bd72c01713e1d
Vitalij Prochazka <vitek.prochazka1@seznam.cz>
2023-02-22 20:09:22 +0100

    Style deleteMovie btn

--- src/components/MovieDeleteButton.js
+++ src/components/MovieDeleteButton.js
@@ -1,7 +1,7 @@
 import "./MovieDeleteButton.css"
 
 const MovieDeleteButton = (props) => {
-    return <button onClick={props.deleteMovie}>Vymazat film</button>
+    return <button className="movie-delete-btn" onClick={props.deleteMovie}>Vymazat film</button>
 }
 
 export default MovieDeleteButton
